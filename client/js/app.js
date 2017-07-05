669ca63cb8d7c3c086a8cd7eb10cc3fac0016c81
THEozmic <michaelozoemena2@gmail.com>
2017-07-05 08:39:09 +0100

    Add webpack build system with scss and babel loaders

---
+++ client/js/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../scss/index.scss';
+import SideMenu from './components/side-menu';
+
+const app = document.getElementById('app');
+ReactDOM.render(<SideMenu/>, app);
---
+++ client/js/components/side-menu.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class SideMenu extends React.Component {
+    render () {
+        return (<div>Hello World</div>);
+    }
+}
