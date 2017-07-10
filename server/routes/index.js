e47407ab32c80f5687e8f654958ad6cb147a681a
THEozmic <michaelozoemena2@gmail.com>
2017-07-10 16:42:25 +0100

    Modify exclusion of signin and signup api routes from jwt middleware

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,24 +2,24 @@ import jwt from 'jsonwebtoken';
 import controllers from '../controllers';
 
 export default (app) => {
+  // API routes for users to create accounts and login to the application
+  app.post('/api/user/signup', controllers.users.create);
+  app.post('/api/user/signin', controllers.users.auth);
+
   app.use((req, res, next) => {
     // exclude these url from this middleware
-    if (req.originalUrl === '/api/user/signin/' || req.originalUrl === '/api/user/signup/') {
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    jwt.verify(token, 'secret', (err, decoded) => {
+      if (err) {
+        res.status(401).send({
+          message: 'user not authenticated, invalid access token'
+        });
+        return;
+      }
+      // if everything is good, save to request for use in other routes
+      req.decoded = decoded;
       next();
-    } else {
-      const token = req.body.token || req.query.token || req.headers['x-access-token'];
-      jwt.verify(token, 'secret', (err, decoded) => {
-        if (err) {
-          res.status(401).send({
-            message: 'user not authenticated, invalid access token'
-          });
-          return;
-        }
-        // if everything is good, save to request for use in other routes
-        req.decoded = decoded;
-        next();
-      });
-    }
+    });
   });
 
   app.get('/api', (req, res) => res.status(200).send({
@@ -30,25 +30,21 @@ export default (app) => {
     message: 'Welcome to the PostIT API'
   }));
 
-  // API routes for users to create accounts and login to the application
-  app.post('/api/user/signup', controllers.users.create);
-  app.post('/api/user/signin', controllers.users.auth);
-
   // API route to get list of all users
-  app.get('/api/user', controllers.users.fetch);
+  app.get('/api/users', controllers.users.fetch);
 
   // API route that allow users create broadcast groups
-  app.post('/api/group/', controllers.groups.create);
+  app.post('/api/groups/', controllers.groups.create);
 
   // API route to get list of all groups
-  app.get('/api/group/', controllers.groups.fetch);
+  app.get('/api/groups/', controllers.groups.fetch);
 
   // API route that allow users add other users to groups
-  app.post('/api/group/:id/user', controllers.groupUsers.addUser);
+  app.post('/api/groups/:id/user', controllers.groupUsers.addUser);
 
   // API route that allows a logged in user post messages to created groups
-  app.post('/api/group/:id/message', controllers.messages.create);
+  app.post('/api/groups/:id/message', controllers.messages.create);
 
   // API route that allows a logged in user retrieve messages from group
-  app.get('/api/group/:id/messages', controllers.groups.messages);
+  app.get('/api/groups/:id/messages', controllers.groups.messages);
 };
