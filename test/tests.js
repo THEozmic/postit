6c9d92eece7a586ac793182b31893299b10f8f2e
THEozmic <michael.ozoemena@andela.com>
2017-08-15 13:09:24 +0100

    fix: Modify test script

--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,4 @@
-import clientTests from '../client/js/spec/client-tests';
+// import clientTests from '../client/js/spec/client-tests';
 import apiTests from '../server/tests/api-tests';
 
-export default { apiTests, clientTests };
+export default { apiTests };
