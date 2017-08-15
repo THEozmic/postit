c689a97181a2c1a4dd2846d21eb8275612d46d12
THEozmic <michael.ozoemena@andela.com>
2017-08-15 13:26:05 +0100

    chore: Modify travis to collect client test coverage

--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,4 @@
 // import clientTests from '../client/js/spec/client-tests';
-import apiTests from '../server/tests/api-tests';
+import apiTests from '../server/tests/api-tests.spec';
 
 export default { apiTests };
