9057ecd2f732ee41777c40b347445c40c06d559e
THEozmic <michael.ozoemena@andela.com>
2017-09-15 16:54:29 +0100

    Remove dead code

--- client/js/components/presentational/MessageBox.jsx
+++ client/js/components/presentational/MessageBox.jsx
@@ -1,26 +1,4 @@
 import React from 'react';
-
-// const MessageBox = (inputs, send, sendStatus) =>
-//   <div className="new-message">
-//     <div className="col-12 pl-0 pr-0 pb-2">
-//       {/* <textarea className="message-box" placeholder="Type your message..."
-//       ref={(input) => { inputs.content = input; }}></textarea> */}
-//     </div>
-//     <div className="col-12 pl-0 pr-0">
-//       <div className="priority-level">
-//         {/* <select className="browser-default" ref={(input) => { inputs.priority = input; }}>
-//           <option value="Normal">Normal</option>
-//           <option value="Urgent">Urgent</option>
-//           <option value="Critical">Critical</option>
-//         </select> */}
-//       </div>
-//       {/* <div className="right">
-//         <button disabled={sendStatus === 'SEND...'}
-//         className="btn btn-primary" onClick={send}>
-//         {sendStatus}</button></div> */}
-//     </div>
-//   </div>;
-
 class MessageBox extends React.Component {
 
   constructor(props) {
