ac901078e4b8dcc7953af68facf2a2d0c293b903
THEozmic <michael.ozoemena@andela.com>
2017-09-15 16:51:37 +0100

    Add Form component to exported components

--- client/js/components/containers/index.js
+++ client/js/components/containers/index.js
@@ -4,5 +4,6 @@ import Dashboard from './Dashboard.jsx';
 import Register from './Register.jsx';
 import Login from './Login.jsx';
 import Header from './Header.jsx';
+import Form from './Form.jsx';
 
-export { Register, Login, Dashboard, Header, Group, Messages };
+export { Register, Login, Dashboard, Header, Group, Messages, Form };
