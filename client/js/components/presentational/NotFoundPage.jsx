c38018ab23203245b58228ab585d523a0512d340
THEozmic <michael.ozoemena@andela.com>
2017-09-19 13:08:57 +0100

    Add 404 page

---
+++ client/js/components/presentational/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <div><h5>Page not found.</h5> <a href="#/dashboard">Go Back?</a></div>
+  );
+};
+
+export default NotFoundPage;
