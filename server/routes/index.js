235e967ce551f0b13b22c82a88c4780ecc9c140b
THEozmic <michael.ozoemena@andela.com>
2017-09-19 13:12:00 +0100

    Remove "read messages" route

--- server/routes/index.js
+++ server/routes/index.js
@@ -43,9 +43,6 @@ export default (app) => {
   // API route that allows a logged in user retrieve messages from group
   app.get('/api/groups/:id/messages/', controllers.groups.findMessages);
 
-  // API route that allows a logged in user retrieve messages from group
-  app.post('/api/groups/:id/read/', controllers.groups.readMessage);
-
   // API route that returns current logged in user and their group(s)
   app.get('/api/users/me/', controllers.users.fetchCurrentUser);
 
