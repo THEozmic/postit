ccc010b72651a9d839caf6a3b6add562d3e2bb9b
THEozmic <michael.ozoemena@andela.com>
2017-10-03 19:08:33 +0100

    Export actions

---
+++ client/js/actions/index.js
@@ -0,0 +1,12 @@
+import changeSelectedGroup from './group';
+import { loadMessages, sendMessage, fetchMessages } from './message';
+import { loginUser, logoutUser, registerUser } from './user';
+
+export {
+  changeSelectedGroup,
+  loadMessages,
+  sendMessage,
+  fetchMessages,
+  loginUser,
+  logoutUser,
+  registerUser };
