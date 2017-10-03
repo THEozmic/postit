a0d9b7dbb2b49218c10262dcfdc0cd482d912b8a
THEozmic <michael.ozoemena@andela.com>
2017-10-03 19:44:07 +0100

    Refactor actions

--- client/js/actions/message.js
+++ client/js/actions/message.js
@@ -1,31 +1,27 @@
 import axios from 'axios';
 import changeSelectedGroup from './group';
 
-export function loadMessages(messages) {
-  return {
-    type: 'LOAD_MESSAGES',
-    messages
-  };
-}
+const loadMessages = messages => ({
+  type: 'LOAD_MESSAGES',
+  messages
+});
 
-export const sendMessage = newMessage => ({
+const sendMessage = newMessage => ({
   type: 'SEND_MESSAGE',
   newMessage
 });
 
-export function fetchMessages(groupId) {
-  return function action(dispatch) {
-    const request = axios({
-      method: 'GET',
-      url: `/api/v1/groups/${groupId}`,
-      headers:
-        { 'x-access-token': JSON.parse(sessionStorage.getItem('user')).token }
-    });
-    return request.then(
-      (response) => {
-        dispatch(loadMessages(response.data.messages));
-        dispatch(changeSelectedGroup(response.data));
-      }
-    );
-  };
-}
+const fetchMessages = groupId => function action(dispatch) {
+  const request = axios({
+    method: 'GET',
+    url: `/api/v1/groups/${groupId}`
+  });
+  return request.then(
+    (response) => {
+      dispatch(loadMessages(response.data.messages));
+      dispatch(changeSelectedGroup(response.data));
+    }
+  );
+};
+
+export { fetchMessages, sendMessage, loadMessages };
