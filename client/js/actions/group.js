99c4bf466e833ba453fbd689ffdc7375ccdb0757
THEozmic <michael.ozoemena@andela.com>
2017-10-15 18:09:11 +0100

    Add search and update members action

--- client/js/actions/group.js
+++ client/js/actions/group.js
@@ -1,6 +1,42 @@
-const changeSelectedGroup = group => ({
+import axios from 'axios';
+
+export const changeSelectedGroup = group => ({
   type: 'CHANGE_SELECTED_GROUP',
   group
 });
 
-export default changeSelectedGroup;
+const search = result => ({
+  type: 'SEARCH',
+  result
+});
+
+export const apiSearch = (groupId, searchTerm, page) =>
+function action(dispatch) {
+  axios.defaults.headers.common['x-access-token'] =
+  sessionStorage.getItem('token');
+  const request = axios({
+    method: 'GET',
+    url: `/api/v1/search/${groupId}/${searchTerm}/${page - 1}`
+  });
+  return request.then(
+    (response) => {
+      dispatch(search(response.data));
+    }
+  );
+};
+
+export const apiUpdateMembers = (selectedUsers, groupId) =>
+function action(dispatch) {
+  axios.defaults.headers.common['x-access-token'] =
+  sessionStorage.getItem('token');
+  const request = axios({
+    method: 'POST',
+    data: { usersIds: selectedUsers },
+    url: `/api/v1/groups/${groupId}/user/`
+  });
+  return request.then(
+    (response) => {
+      dispatch(search(response.data));
+    }
+  );
+};
