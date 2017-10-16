368fada3ca9eab2a04f0f0e0143d8a229f5fb6aa
THEozmic <michael.ozoemena@andela.com>
2017-10-16 04:20:17 +0100

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
+  devtool: 'inline-sourcemap',
   entry: './client/js/App.jsx',
   node: {
     net: 'empty',
