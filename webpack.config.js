25075ce4ea0a31315970382e4ece7f46c4756efd
THEozmic <michael.ozoemena@andela.com>
2017-10-16 05:18:01 +0100

    Udpate webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  devtool: 'inline-sourcemap',
+  devtool: debug ? 'inline-sourcemap' : false,
   entry: './client/js/App.jsx',
   node: {
     net: 'empty',
