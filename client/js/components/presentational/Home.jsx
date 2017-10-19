c61b12bd659bb631425b01fbe6d3c1dfeebd5f3e
THEozmic <michael.ozoemena@andela.com>
2017-10-19 20:09:35 +0100

    Update home text and jsdoc

--- client/js/components/presentational/Home.jsx
+++ client/js/components/presentational/Home.jsx
@@ -3,33 +3,35 @@ import { Link } from 'react-router-dom';
 import { Footer, Header } from './';
 
 /**
- * Home component
- * @returns {JSX} for the Home component
+ * Home Component
+ * @method Home
+ * @returns {Object} JSX
+ * @param {Object} props
  */
 const Home = () =>
   (<div>
     <Header />
     <section className="container">
       <div>
-        <h5>
+        <h5 className="intro-text">
           <p className="mt-5">
-            A web application
-            that lets you easily share
-            messages to people you care about.
+            Now you can send <span className="important">prioritized </span>
+            broadcasts to people who need to recieve them!
           </p>
         </h5>
         <div className="steps">
+          <h4 className="title">Get started</h4>
           <div> 1. Create an account</div>
-          <div> 2. Create a message board</div>
-          <div> 3. Add friends to board</div>
-          <div> 4. Post away!</div>
+          <div> 2. Create a group</div>
+          <div> 3. Add people to the group</div>
+          <div> 4. Post Broadcasts that matter</div>
         </div>
         <div className="action-buttons">
           <Link
             id="create-account"
             to="register"
             className="waves-effect waves-light btn action-btn"
-          >Create an account</Link>
+          >Start Here</Link>
           <Link
             id="login"
             to="login"
