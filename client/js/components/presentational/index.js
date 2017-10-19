a1168a9f876454abc8ec7d540a44366d7fd6236a
THEozmic <michael.ozoemena@andela.com>
2017-10-19 20:10:07 +0100

    Update component import

--- client/js/components/presentational/index.js
+++ client/js/components/presentational/index.js
@@ -2,19 +2,11 @@ import Home from './Home';
 import Groups from './Groups';
 import Header from './Header';
 import Footer from './Footer';
-import NewGroup from '../containers/NewGroup';
-import Search from './Search';
-import Recover from '../containers/RecoverPassword';
-import NewPassword from '../containers/NewPassword';
-import SideMenu from './Sidemenu';
-import Form from '../containers/Form';
-import MessageBox from './MessageBox';
 import Message from './Message';
 import NotFoundPage from './NotFoundPage';
+import SideMenu from './SideMenu';
 
 export
 {
-  Home, Header, Groups, Footer, Search,
-  NewGroup, Recover, NewPassword, SideMenu,
-  Form, MessageBox, Message, NotFoundPage
+  Home, Header, Groups, Footer, Message, NotFoundPage, SideMenu
 };
