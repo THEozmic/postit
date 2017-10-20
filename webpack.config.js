16626e6bcc18c7cd6feca2229fe5a59193b64ed4
THEozmic <michael.ozoemena@andela.com>
2017-10-20 07:01:38 +0100

    Update webpack config'

--- webpack.config.js
+++ webpack.config.js
@@ -48,13 +48,13 @@ module.exports = {
       'window.jQuery': 'jquery',
       Hammer: 'hammerjs/hammer'
     }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-      }
-    }),
+    // new webpack.DefinePlugin({
+    //   'process.env': {
+    //     NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+    //   }
+    // }),
     new ExtractTextPlugin({ filename: 'style.css', allChunks: true }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: true })
+    // new webpack.optimize.OccurrenceOrderPlugin(),
+    // new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: true })
   ],
 };
