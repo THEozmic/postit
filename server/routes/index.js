bc59b652a9ef43ef8cdbe55bdd9e82d13528f45f
Michael Ozoemena <michaelozoemena2@gmail.com>
2017-11-01 13:24:45 +0100

    Update routes/index.js
    WIP: fix hound errors

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,13 +2,18 @@ import controllers from '../controllers';
 import verifyToken from '../helpers/verifyToken';
 
 export default (app) => {
-  // API routes for users to create accounts and login to the application
+  // API route for users to create account
   app.post('/api/v1/users/', controllers.users.createUser);
+
+  // API routes for users to authenticate user
   app.post('/api/v1/users/signin/', controllers.users.authenticateUser);
+
   // API route to request for new password
   app.post('/api/v1/users/request-password', controllers.users.passwordRequest);
+
   // API route to reset password
   app.post('/api/v1/users/reset-password/:hash',
+
     controllers.users.updatePassword);
 
   // API route that allow users create broadcast groups
@@ -16,6 +21,7 @@ export default (app) => {
 
   // API route that allow users add/remove other users to/from groups
   app.post('/api/v1/groups/:id/user/', verifyToken,
+
     controllers.groupUsers.addOrRemoveUser);
 
   // API route to get list of all groups
@@ -23,10 +29,12 @@ export default (app) => {
 
   // API route that allows a logged in user post messages to created groups
   app.post('/api/v1/groups/:id/message/', verifyToken,
+
     controllers.messages.createMessage);
 
   // API route that allows a logged in user retrieve messages from group
   app.get('/api/v1/groups/:id/messages/', verifyToken,
+
     controllers.groups.findMessages);
 
   // API route that returns current logged in user and their group(s)
@@ -34,6 +42,7 @@ export default (app) => {
 
   // API route for search
   app.get('/api/v1/search/:group/:query/:page', verifyToken,
+
     controllers.users.searchUsers);
 
   // This should always go last
