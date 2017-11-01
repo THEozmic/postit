08ad0ef803d0e7ce9f94c859af6c6865bbedaa86
Michael Ozoemena <michaelozoemena2@gmail.com>
2017-11-01 13:15:07 +0100

    Update routes/index.js
    Use verifyToken middleware in protected routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
-import jwt from 'jsonwebtoken';
 import controllers from '../controllers';
+import verifyToken from '../helpers/verifyToken';
 
 export default (app) => {
   // API routes for users to create accounts and login to the application
@@ -10,42 +10,31 @@ export default (app) => {
   // API route to reset password
   app.post('/api/v1/users/reset-password/:hash',
   controllers.users.updatePassword);
-  let token;
-  app.use((req, res, next) => {
-    token = req.headers['x-access-token'];
-    jwt.verify(token, process.env.JWT_TOKEN || 'SECRET', (err, decoded) => {
-      if (err) {
-        res.status(401).send({
-          error: { message: 'user not authenticated, invalid access token' }
-        });
-        return;
-      }
-      // if everything is good, save to request for use in other routes
-      req.decoded = decoded;
-      next();
-    });
-  });
 
   // API route that allow users create broadcast groups
-  app.post('/api/v1/groups/', controllers.groups.createGroup);
+  app.post('/api/v1/groups/', verifyToken, controllers.groups.createGroup);
 
   // API route that allow users add/remove other users to/from groups
-  app.post('/api/v1/groups/:id/user/', controllers.groupUsers.addOrRemoveUser);
+  app.post('/api/v1/groups/:id/user/', verifyToken,
+  controllers.groupUsers.addOrRemoveUser);
 
   // API route to get list of all groups
-  app.get('/api/v1/groups/:id', controllers.groups.fetchGroups);
+  app.get('/api/v1/groups/:id', verifyToken, controllers.groups.fetchGroups);
 
   // API route that allows a logged in user post messages to created groups
-  app.post('/api/v1/groups/:id/message/', controllers.messages.createMessage);
+  app.post('/api/v1/groups/:id/message/', verifyToken,
+  controllers.messages.createMessage);
 
   // API route that allows a logged in user retrieve messages from group
-  app.get('/api/v1/groups/:id/messages/', controllers.groups.findMessages);
+  app.get('/api/v1/groups/:id/messages/', verifyToken,
+  controllers.groups.findMessages);
 
   // API route that returns current logged in user and their group(s)
-  app.get('/api/v1/users/me/', controllers.users.fetchCurrentUser);
+  app.get('/api/v1/users/me/', verifyToken, controllers.users.fetchCurrentUser);
 
   // API route for search
-  app.get('/api/v1/search/:group/:query/:page', controllers.users.searchUsers);
+  app.get('/api/v1/search/:group/:query/:page', verifyToken,
+  controllers.users.searchUsers);
 
   // This should always go last
   app.all('/*', (req, res) => res.status(404).send({
